d619dc4c68cf3eb431f58e50fc03d58bd7ac5f2a
Dj157 <sablesdmj@gmail.com>
2024-11-10 18:45:01 -0300

    Create App.js

---
+++ ClinicaOtohAPI/ClinicaOtoh1.0-main/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import PacientesList from './PacientesList';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+      <PacientesList />
+    </div>
+  );
+}
+
+export default App;
